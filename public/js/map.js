98eaaa8ad1226c0445bce9370d2a799825e31ed7
Gangadhar Shaw <gangadharshaw7@gmail.com>
2024-08-27 12:20:25 +0530

    add favicon

--- public/js/map.js
+++
@@ -1,6 +0,0 @@
-mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
-  container: "map", // container ID
-  center: [77.209, 28.6139], // starting position [lng, lat]
-  zoom: 9, // starting zoom
-});
